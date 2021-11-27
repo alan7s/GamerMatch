561c25eb874fcfae2eef8ce7d1ac7ecac4f7e4bc
Hortigas <xxvsvitor98xx@gmail.com>
2021-11-27 17:04:10 -0300

    Landing-Page-1

---
+++ gamer-match/src/pages/Landing/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logoIMG from "../../assets/logoImg2.png"
+import iconPurpleHeart from "../../assets/icons/circle-purple-heart.svg"
+
+function Landing() {
+   return (
+      <div id="page-landing">
+         <div id="page-landing-content" className="container">
+            <div className="logo-container">
+               <h1>Gamer Match</h1>
+               <h2>Encontre seu duo.</h2>
+            </div>
+            <img src={logoIMG} alt="logo Gamer match" className="hero-image" />
+            <div className="buttons-container">
+               <a href="" className="signIn">Logar</a>
+               <a href="" className="signUp">Cadastrar</a>
+            </div>
+            <span className="total-connections">total de 200 conexões já realizadas <img src={iconPurpleHeart} alt="coração roxo" />
+            </span>
+         </div>
+      </div>
+   )
+}
+
+export default Landing;

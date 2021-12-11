444bdbc55ef671d4879917d5f3b8472d032949d3
alan7s <alan7@outlook.com>
2021-12-11 16:16:02 -0300

    Add página recuperação de senha e add bordas redondas nos input

--- gamer-match/src/App.tsx
+++ gamer-match/src/App.tsx
@@ -3,10 +3,11 @@ import './styles/global.css'
 import Landing from './pages/Landing';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Recovery from './pages/SignIn/Recovery';
 
 function App() {
   return (
-    <SignIn/>
+    <Landing/>
   );
 }
 

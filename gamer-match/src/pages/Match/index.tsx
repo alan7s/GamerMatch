e99f5abf7168162ae6e6ea9cf97a63aeffd07dd3
Hortigas <xxvsvitor98xx@gmail.com>
2022-01-12 16:33:17 -0300

    match update 2

--- gamer-match/src/pages/Match/index.tsx
+++ gamer-match/src/pages/Match/index.tsx
@@ -1,34 +1,38 @@
 import React from 'react';
 
 import logoIMG from '../../assets/logoImg2.png';
-import play from '../../assets/icons/play.png';
+import dots from '../../assets/icons/dots.png';
 import no from '../../assets/icons/close.png';
-import perfilPic from '../../assets/userpic/userpic1.jpg';
 import userpic1 from '../../assets/userpic/fakePic.png';
-import userpic2 from '../../assets/userpic/userpic2.jpg';
-import userpic3 from '../../assets/userpic/userpic3.jpg';
-import userpic4 from '../../assets/userpic/userpic4.jpg';
 import './styles.css';
 
 function Match() {
     return (
         <div id="page-match">
-            <div id="page-match-content" className="container">
+            <div id="page-match-content">
                 <div className="header">
                     <img src={logoIMG} alt="" />
                 </div>
                 <div className="card">
-                    <img src={userpic1} alt="" />
-                    <div id="ball"></div>
+                    <div id="cardShadow"></div>
+                    <img id="cardPic" src={userpic1} alt="" />
                     <div className="buttons-content">
                         <div className="buttons" id="passB">
                             <img src={no} alt="" />
                         </div>
-                        <div className="buttons" id="likeB"></div>
-                        <div className="buttons" id="perfilB"></div>
+                        <div className="buttons" id="likeB">
+                            I'm Game!
+                        </div>
+                        <div className="buttons" id="perfilB">
+                            <img src={dots} alt="" />
+                        </div>
                     </div>
                 </div>
-                <div className=" footer">
+                <div className="description">
+                    <h3>Purutchuda, 24</h3>
+                </div>
+                <div className="footer">
+                    <div id="ball"></div>
                     <a href=""></a>
                     <a href=""></a>
                     <a href=""></a>

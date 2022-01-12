d275b7ea6a5356d2fa0e68ba19b7590079b8812b
Hortigas <xxvsvitor98xx@gmail.com>
2022-01-12 18:31:38 -0300

    correção

--- gamer-match/src/pages/Chat/index.tsx
+++ gamer-match/src/pages/Chat/index.tsx
@@ -6,7 +6,6 @@ import userpic1 from '../../assets/userpic/fakePic.png';
 import userpic2 from '../../assets/userpic/userpic2.jpg';
 import userpic3 from '../../assets/userpic/userpic3.jpg';
 import userpic4 from '../../assets/userpic/userpic4.jpg';
-import close from '../../assets/icons/closecinza.png'
 import close from '../../assets/icons/closecinza.png';
 import './styles.css';
 
@@ -30,25 +29,34 @@ function ChatBox() {
         <div id="page-chat">
             <div id="page-chat-content">
                 <div className="header">
-                    <img src={logoIMG} alt="" />
+                    <img src="{logoIMG}" alt="" />
                 </div>
                 <div className="card">
                     <div className="show" id="msgbox">
                         <div className="chat-title">Mensagens</div>
                         <div className="messages" id="messages">
-                            <div className="message" id="message" onClick={showMsg}><img src={userpic1} alt="" /><span>Fulano</span></div>
-                            <div className="message" id="message" onClick={showMsg}><img src={userpic2} alt="" /><span>Ciclano</span></div>
-                            <div className="message" id="message" onClick={showMsg}><img src={userpic3} alt="" /><span>Beltrano</span></div>
                             <div className="message" id="message" onClick={showMsg}>
-                                <img src={userpic1} alt="" />
+                                <img src="{userpic1}" alt="" />
                                 <span>Fulano</span>
                             </div>
                             <div className="message" id="message" onClick={showMsg}>
-                                <img src={userpic2} alt="" />
+                                <img src="{userpic2}" alt="" />
                                 <span>Ciclano</span>
                             </div>
                             <div className="message" id="message" onClick={showMsg}>
-                                <img src={userpic3} alt="" />
+                                <img src="{userpic3}" alt="" />
+                                <span>Beltrano</span>
+                            </div>
+                            <div className="message" id="message" onClick={showMsg}>
+                                <img src="{userpic1}" alt="" />
+                                <span>Fulano</span>
+                            </div>
+                            <div className="message" id="message" onClick={showMsg}>
+                                <img src="{userpic2}" alt="" />
+                                <span>Ciclano</span>
+                            </div>
+                            <div className="message" id="message" onClick={showMsg}>
+                                <img src="{userpic3}" alt="" />
                                 <span>Beltrano</span>
                             </div>
                         </div>
@@ -56,11 +64,11 @@ function ChatBox() {
                     <div className="chatbox hide" id="chatbox">
                         <div className="chatbox-title">
                             <div className="chatbox-title-left">
-                                <img id="chatbox-user-avatar" src={userpic1} alt="" />
+                                <img id="chatbox-user-avatar" src="{userpic1}" alt="" />
                                 <span id="chatbox-user-name">Fulano</span>
                             </div>
                             <div className="chatbox-title-right">
-                                <img id="chatbox-close" src={close} onClick={showMsg} />
+                                <img id="chatbox-close" src="{close}" onClick={showMsg} />
                             </div>
                         </div>
                         <div className="message-container">
@@ -70,50 +78,39 @@ function ChatBox() {
                                         <p>Olá Fulano, como vai a família ? E o trem tu pegou? Não sei se poderei jogar.</p>
                                     </div>
                                     <div className="message-avatar">
-                                        <img src={userpic4} />
+                                        <img src="{userpic4}" />
                                     </div>
                                 </div>
-                                <div className='message-left'>
                                 <div className="message-left">
                                     <div className="message-left">
+                                        <div className="message-left">
+                                            <div className="message-avatar">
+                                                <img src="{userpic1}" />
+                                            </div>
+                                            <div className="message-content message-content-left">
+                                                <p>Peguei sim, tranquilo.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="message-right">
+                                        <div className="message-content message-content-right">
+                                            <p>Nice!</p>
+                                        </div>
                                         <div className="message-avatar">
-                                            <img src={userpic1} />
+                                            <img src="{userpic4}" />
                                         </div>
-                                        <div className="message-content message-content-left">
-                                            <p>Peguei sim, tranquilo.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="message-right">
-                                    <div className="message-content message-content-right">
-                                        <p>Nice!</p>
-                                    </div>
-                                    <div className="message-avatar">
-                                        <img src={userpic4} />
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="chatbox-input">
-                            <input placeholder="Digitar..." id="message-input" />
+                            <div className="chatbox-input">
+                                <input placeholder="Digitar..." id="message-input" />
+                            </div>
                         </div>
                     </div>
-                </div>
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-                {/*teste<div className="footer">
-=======
-                {/*<div className="footer">
->>>>>>> parent of 67f4102 (Refatorando pagina chat)
+
+                    <div className="footer"></div>
                     <div id="ball"></div>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-    </div>*/}
-=======
-                <div className="footer"></div>
-                <div id="ball"></div>
->>>>>>> Stashed changes
+                </div>
             </div>
         </div>
     );
